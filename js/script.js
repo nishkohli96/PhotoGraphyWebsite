90f1949a559959c15fa7e2785ebe61157067a447
Nishant Kohli <nishkohli96@gmail.com>
2019-12-16 05:06:48 +0530

    Latest code

--- js/script.js
+++ js/script.js
@@ -1,61 +1,67 @@
 
-  var slideIndex = 1;
-  var newIndex = slideIndex;
+var slideIndex = 0;
+var newIndex = slideIndex;
 
-  $("#link_nature_pics").click(function(){
-    slideIndex = 1;
-    showSlides(slideIndex);
-  });
+$("#link_nature_pics").click(function(){
+  slideIndex = 0;
+  newIndex = slideIndex;
+  showSlides(slideIndex);
+});
 
-  $("#link_wildlife_pics").click(function(){
-    slideIndex = 7;
-    showSlides(slideIndex);
-  });
+$("#link_wildlife_pics").click(function(){
+  slideIndex = 6;
+  newIndex = slideIndex;
+  showSlides(slideIndex);
+});
 
-  $("#link_landmark_pics").click(function(){
-    slideIndex = 13;
-    showSlides(slideIndex);
-  });
+$("#link_landmark_pics").click(function(){
+  slideIndex = 12;
+  newIndex = slideIndex;
+  showSlides(slideIndex);
+});
 
-  $("#link_human_pics").click(function(){
-    slideIndex = 19;
-    showSlides(slideIndex);
-  });
+$("#link_human_pics").click(function(){
+  slideIndex = 18;
+  newIndex = slideIndex;
+  showSlides(slideIndex);
+});
 
-  $("#link_random_pics").click(function(){
-    slideIndex = 25;
-    showSlides(slideIndex);
-  });
+$("#link_random_pics").click(function(){
+  slideIndex = 24;
+  newIndex = slideIndex;
+  showSlides(slideIndex);
+});
 
-  // Next/previous controls
-  function plusSlides(n) 
+// Next/previous controls
+function plusSlides(n) 
+{
+  newIndex = newIndex + n ;
+  if (newIndex > slideIndex + 5) { newIndex = slideIndex }
+  if (newIndex < slideIndex) { newIndex = slideIndex + 5 }
+  showSlides(newIndex);
+}
+
+// Thumbnail image controls
+function showSlides(n) 
+{
+  var i;
+  var slides = document.getElementsByClassName("gallerySlides");
+  var dots = document.getElementsByClassName("demo");
+
+  for (i = 0; i < slides.length; i++) 
   {
-    newIndex = newIndex + n ;
-    if (newIndex > slideIndex + 5) { newIndex = slideIndex }
-    if (newIndex < slideIndex) { newIndex = slideIndex + 5 }
-    showSlides(newIndex);
+    slides[i].style.display = "none";
   }
 
-  // Thumbnail image controls
- 
-  function showSlides(n) 
+  for (i = 0; i < dots.length; i++) 
   {
-    var i;
-    var slides = document.getElementsByClassName("gallerySlides");
-    var dots = document.getElementsByClassName("demo");
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[n].style.display = "block";
+  dots[n].className += " active";
+}
 
-    for (i = 0; i < slides.length; i++) 
-    {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) 
-    {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[n-1].style.display = "block";
-    dots[n-1].className += " active";
-  }
-  function currentSlide(n) 
-  {
-    showSlides(slideIndex = n);
-  }
+function currentSlide(n) 
+{
+  showSlides(slideIndex = n);
+}
